596411edc19b12ca7a38ef7e16a6c12d08588995
Mark Alston <marktalston@gmail.com>
2025-04-02 18:51:47 -0600

    Update demo_release_pipeline.py to show help without arguments
    - Update demo_release_pipeline.py to use HelpfulArgumentParser
    - Makes all scripts in the codebase show help text when run without arguments

--- src/demo_release_pipeline.py
+++ src/demo_release_pipeline.py
@@ -7,22 +7,11 @@ import subprocess
 import sys
 from typing import Optional
 
+from helpers.argparse_helper import CustomHelpFormatter, HelpfulArgumentParser
 from helpers.concourse import ConcourseClient
 from helpers.git_helper import GitHelper
 from helpers.logger import default_logger as logger
 from helpers.release_helper import ReleaseHelper
-
-
-class CustomHelpFormatter(argparse.RawDescriptionHelpFormatter):
-    """Custom help formatter to modify the help output."""
-
-    def format_help(self):
-        help_text = super().format_help()
-        # Remove the default options section
-        help_text = help_text.split("\n\n")[0] + "\n\n" + help_text.split("\n\n")[-1]
-        # Change "usage:" to "Usage:"
-        help_text = help_text.replace("usage:", "Usage:")
-        return help_text
 
 
 class DemoReleasePipeline:
@@ -614,7 +603,7 @@ class DemoReleasePipeline:
 
 def main():
     """Main function to parse arguments and run the demo release pipeline."""
-    parser = argparse.ArgumentParser(
+    parser = HelpfulArgumentParser(
         prog="demo_release_pipeline.py",
         description="Demo release pipeline script",
         formatter_class=CustomHelpFormatter,
